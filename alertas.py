9c41469fa41390f3e0f28e69610427632e0bd37b
0kevin4 <morenoramirezkevinjose@gmail.com>
2026-05-06 12:34:26 -0500

    correcion_correo_enviar

--- alertas.py
+++ alertas.py
@@ -44,7 +44,7 @@ def enviar_correo(asunto, html):
         },
         json={
             "from": "onboarding@resend.dev",
-            "to": ["morenoramirezkevinjose@gmail.com"],
+            "to": "morenoramirezkevinjose@gmail.com",
             "subject": asunto,
             "html": html,
         },
